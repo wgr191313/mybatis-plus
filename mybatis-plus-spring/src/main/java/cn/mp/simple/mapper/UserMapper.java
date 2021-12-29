b2f052d7452a34ea6d505173f60c514087fafbea
yeyuyue <xiaoxuandashen@163.com>
2021-12-29 18:06:55 +0800

    mybatis-plus+spring整合

---
+++ mybatis-plus-spring/src/main/java/cn/mp/simple/mapper/UserMapper.java
@@ -0,0 +1,12 @@
+package cn.mp.simple.mapper;
+
+import cn.mp.simple.pojo.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * @author wangguanru
+ * @date 2021-12-29
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
---
+++ mybatis-plus-spring/src/main/java/cn/mp/simple/pojo/User.java
@@ -0,0 +1,23 @@
+package cn.mp.simple.pojo;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author wangguanru
+ * @date 2021-12-29
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@TableName("tb_user")
+public class User {
+    private Long id;
+    private String userName;
+    private String password;
+    private String name;
+    private Integer age;
+    private String email;
+}
